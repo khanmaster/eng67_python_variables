28efc390caa6886facf652595ce0aad363f79b5e
khanmaster <eashkhan@hotmail.com>
2020-06-30 17:22:09 +0100

    python variables and task

---
+++ variables.py
@@ -0,0 +1,45 @@
+
+# print(" hello world ")
+
+# Let's create a variable
+
+# age = 99 # type of integer
+# NHS = 12344555 # float
+#
+# name = input("please enter your name ")
+# age = input("Please enter your age ")
+# print(name)
+# print(age)
+
+# overwriting a variable
+name = "jame"
+print(name)
+name = "bond"
+print(name)
+
+#
+#
+# print(x)
+# print(x + y)
+# print(name)
+#
+# casting an int to string
+# print(str(x) + name)
+#
+# add the variable called x and variable called name and display it
+#
+# builtin method called type()
+
+# exercise - capturing user details -
+
+# create a variable called first_name and last_name
+
+# create a variable called full_name and display full_name
+
+# create a variable called age
+
+# create a variable called address
+
+# prompt the user the get all the above information
+
+# display all the info collected from the user
